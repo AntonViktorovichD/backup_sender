363a8e8e1b02025cf5ab287bdc32bd1ef4fec94e
Anton Viktorovich <75506917+AntonViktorovichD@users.noreply.github.com>
2023-03-27 13:18:11 +0300

    Update backup_sender.py

--- backup_sender.py
+++ backup_sender.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 
 import os
+import concurrent.futures
+from functools import partial
 
-for item in os.listdir('/home/User/web/backup'):
-    if item.find('20230215') != -1:
-        os.system('sudo rsync -av /home/User/web/backup/' + item + ' **********@**********:/home/grand/web/grandgold.jewelry/public_html/bitrix/backup/')
-        print(item)
+def backup_rsync(item, source_dir, target_dir, ssh_port):
+    if '20230321' in item:
+        try:
+            os.system(f'sudo rsync -av {source_dir}/{item} -e "ssh -p {ssh_port}" root@185.154.52.238:{target_dir}/{item}')
+            print(item)
+        except Exception as e:
+            print(f"Ошибка при выполнении rsync для элемента {item}: {e}")
+
+def main():
+    source_dir = '/home/User/web/backup'
+    target_dir = '/home/grand/web/grand.gold/public_html/bitrix/backup'
+    ssh_port = 63450
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        backup_func = partial(backup_rsync, source_dir=source_dir, target_dir=target_dir, ssh_port=ssh_port)
+        executor.map(backup_func, os.listdir(source_dir))
+
+if __name__ == "__main__":
+    main()
